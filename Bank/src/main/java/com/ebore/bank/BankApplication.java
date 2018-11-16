38243e9ad8cb4cfe8949d08ca3c3e95d589aa9c0
mikeyindev <mikeyindev@gmail.com>
2018-11-16 16:01:56 -0600

    Added Eureka client

--- Bank/src/main/java/com/ebore/bank/BankApplication.java
+++ Bank/src/main/java/com/ebore/bank/BankApplication.java
@@ -1,10 +1,11 @@
 package com.ebore.bank;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BankApplication {
 
 	public static void main(String[] args) {
