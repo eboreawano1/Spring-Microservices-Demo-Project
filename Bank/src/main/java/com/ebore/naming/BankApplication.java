6c9508e4529da8c8a19d332d85e15130334d981a
mikeyindev <mikeyindev@gmail.com>
2018-11-19 11:29:03 -0600

    Added string

--- Bank/src/main/java/com/ebore/naming/BankApplication.java
+++ Bank/src/main/java/com/ebore/naming/BankApplication.java
@@ -1,5 +1,4 @@
 package com.ebore.naming;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -10,6 +9,6 @@ public class BankApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankApplication.class, args);
-		System.out.println("naming server");
+		System.out.println("naming server test");
 	}
 }
